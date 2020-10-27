f25c26c6c40ec560d2957ec4693653acb8713027
ahmf200 <fahad15@live.co.uk>
2020-10-27 14:20:34 +0000

    modified port-scan.py (#4)
    Co-authored-by: Fahad Ahmed <fahadahmed@fahads-mbp.home>

--- port-scan.py
+++ port-scan.py
@@ -1,12 +1,23 @@
 import socket as sk
+import time
 
-ip = input('Enter the IP to scan: \n')
+def scan_port(min_port_range, max_port_range):
+    ip = input('Enter the IP to scan: \n')
+    start = time.perf_counter()
+    for port in range(min_port_range, max_port_range):
+        try:
+            createdSocket = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
+            createdSocket.settimeout(2000)
+            createdSocket.connect((ip, port))
+            print("{}:OPEN".format(port))
+            createdSocket.close
+        except: continue
+    finish = time.perf_counter()
+    print(f'Finished in {round(finish - start, 3)}')
 
-for port in range(1,1024):
-    try:
-        s = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
-        s.settimeout(5000)
-        s.connect((ip, port))
-        print('%d:OPEN' % (port))
-        s.close
-    except: continue
+def main():
+    scan_port(1, 1024)
+
+if __name__ == "__main__":
+    main()
+
